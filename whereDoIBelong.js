78a7c4211bb31e445ea96d474221477f78ff7665
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-11-02 02:28:16 +0900

    Added Comment

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -1,14 +1,18 @@
 /* Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).*/
 
 function getIndexToIns(arr, num) {
+  // Sorts array from greatest to least
   arr.sort(function(a, b) {
     return a - b;
   });
+  //Goes (iterates) through the array
   for (var i = 0; i < arr.length; i++) {
+    //Returns index if array value is greater than or equal to num
     if (arr[i] >= num) {
       return arr.indexOf(arr[i]);
     }
   }
+  //Otherwise just return the array's length
   return arr.length;
 }
 
