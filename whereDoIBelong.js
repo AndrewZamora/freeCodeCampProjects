341da68b8bfd2cab153214d80ff761249923cfff
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-11-02 01:44:18 +0900

    Added for-loop if statement and return

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -4,17 +4,15 @@ function getIndexToIns(arr, num) {
   arr.sort(function(a, b) {
     return a - b;
   });
-for (var i = 0; i < arr.length; i++) {
-if (arr[i] >= num) {
-  console.log(arr.indexOf(arr[i]));
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+      return arr.indexOf(arr[i]);
+    }
+  }
+  return arr.length;
 }
 
-}
-  console.log(arr);
-  return num;
-}
-
-console.log(getIndexToIns([40, 60], 50));
+console.log(getIndexToIns([2, 5, 10], 15));
 
 /*Test Values:
 getIndexToIns([10, 20, 30, 40, 50], 35) should return 3.
