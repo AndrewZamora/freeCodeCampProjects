06a887c04873ad6e7f39c619a200cf650c13fa4b
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-11-04 02:21:55 +0900

    Added charCodeAt Method and console.log

--- ceasarsCipher.js
+++ ceasarsCipher.js
@@ -5,10 +5,10 @@
 // Write a function which takes a ROT13 encoded string as input and returns a decoded string.
 //
 // All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
-function rot13(str) { // LBH QVQ VG!
+function rot13(str) { 
 
-  return str;
+  return str.charCodeAt(0);
 }
 
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"));
