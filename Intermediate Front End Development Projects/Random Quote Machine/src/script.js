510fb8295dc5727d749edf98ba9665f3a64de418
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-11-11 23:32:55 +0900

    Added Jquery CDN to index.html

--- Intermediate Front End Development Projects/Random Quote Machine/src/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/src/script.js
@@ -1,3 +1,5 @@
-$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (a) {
-    $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-});
+alert('hey');
+
+// $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (a) {
+//     $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+// });
