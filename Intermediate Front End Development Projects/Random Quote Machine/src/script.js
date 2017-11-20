a925e3b8704359f4e89e26472a79defadc03cd80
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-11-20 22:54:12 +0900

    Added span for random quote and linked jquery cdn properly

--- Intermediate Front End Development Projects/Random Quote Machine/src/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/src/script.js
@@ -1,5 +1,3 @@
-alert('hey');
-
-// $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function (a) {
-//     $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-// });
+$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(a) {
+    $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+  });
