a257f7af927a71a87f2ea9f9e271c755a620e4eb
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-11-22 12:50:56 +0900

    Added button styles

--- Intermediate Front End Development Projects/Random Quote Machine/src/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/src/script.js
@@ -1,3 +1,4 @@
-$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(a) {
+$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
     $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
   });
+  
