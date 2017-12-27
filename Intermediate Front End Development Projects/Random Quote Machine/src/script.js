920325894a5d17a352b865c3e2f2788106a7d462
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-12-27 23:54:23 +0900

    Added XMLHttpRequest to script.js

--- Intermediate Front End Development Projects/Random Quote Machine/src/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/src/script.js
@@ -1,4 +1,14 @@
-$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-    $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-  });
+// $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback", function(a) {
+//     $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+//   });
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(xhttp.responseText);
+      console.log(response[0].content);
+      document.getElementById('quote').innerHTML = response[0].content;
+    }
+};
+xhttp.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback", true);
+xhttp.send();
   
