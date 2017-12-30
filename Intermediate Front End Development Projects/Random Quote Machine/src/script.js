1adeb1199b73d71e9587cfac09b5a160a6c73585
AndrewZamora <andrewmichaelzamora@gmail.com>
2017-12-30 18:27:32 +0900

    Add shareable content for social media links and got new quotes to be generated

--- Intermediate Front End Development Projects/Random Quote Machine/src/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/src/script.js
@@ -1,14 +1,30 @@
-// $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback", function(a) {
-//     $("#quote").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
-//   });
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+var spaceForQuote = document.getElementById('spaceForQuote');
+var button = document.getElementById('btn');
+var twitterLink = document.getElementById('twitter');
+var tumblrLink = document.getElementById('tumblr');
+//Global variables
+function makeQuote() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var response = JSON.parse(xhttp.responseText);
-      console.log(response[0].content);
-      document.getElementById('quote').innerHTML = response[0].content;
+      var randomNum = Math.floor((Math.random() * 19) + 1);
+      //This is a workaround because I could only call 19 quotes at a time
+      var quote = response[randomNum].content;
+      var author = response[randomNum].title;
+      spaceForQuote.innerHTML = quote + " -" + author;
+      // Creates Quote
+      var plainText = spaceForQuote.innerText.replace(/-/g , " &hashtags=");
+      twitterLink.setAttribute("href", "https://twitter.com/intent/tweet?text=" + plainText);
+      tumblrLink.setAttribute("href", "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=" + author + "&caption=" + author + "&content=" + plainText + "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button");
+      //Creates Social Media links
     }
-};
-xhttp.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback", true);
-xhttp.send();
-  
+  };
+  xhttp.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=19&callback", true);
+  //19(20 and greater don't seem to work) had to be called because of caching
+  xhttp.send();
+}
+// You might have to add "=?" to "&callback" in the GET request Url depending on whether you are working locally or on codepen
+makeQuote();
+//Show initial quote on first page load
+button.addEventListener('click', makeQuote);
