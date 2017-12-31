b066353dfc3db1e586d92b62c03816d1118c9492
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-01-01 03:32:41 +0900

    Finished styles for Random Quote Generator

--- Intermediate Front End Development Projects/Random Quote Machine/src/script.js
+++ Intermediate Front End Development Projects/Random Quote Machine/src/script.js
@@ -12,9 +12,9 @@ function makeQuote() {
       //This is a workaround because I could only call 19 quotes at a time
       var quote = response[randomNum].content;
       var author = response[randomNum].title;
-      spaceForQuote.innerHTML = quote + " -" + author;
+      spaceForQuote.innerHTML = quote + "- " + "<span>" + author + "</span>";
       // Creates Quote
-      var plainText = spaceForQuote.innerText.replace(/-/g , " &hashtags=");
+      var plainText = spaceForQuote.innerText.replace(/-/g, "&hashtags=");
       twitterLink.setAttribute("href", "https://twitter.com/intent/tweet?text=" + plainText);
       tumblrLink.setAttribute("href", "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=" + author + "&caption=" + author + "&content=" + plainText + "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button");
       //Creates Social Media links
