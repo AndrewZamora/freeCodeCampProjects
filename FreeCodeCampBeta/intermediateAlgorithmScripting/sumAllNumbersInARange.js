c15c0aed4ee4f649be8f1680e5a2a536fd7708d6
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-01-24 19:20:14 +0900

    Added Sum All Numbers in a Range Challenge

---
+++ FreeCodeCampBeta/intermediateAlgorithmScripting/sumAllNumbersInARange.js
@@ -0,0 +1,18 @@
+// Intermediate Algorithm Scripting: Sum All Numbers in a Range
+
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+
+// The lowest number will not always come first.
+
+function sumAll(arr) {
+    return 1;
+  }
+  
+  
+
+// Test Cases:
+//   sumAll([1, 4]) should return a number.
+//   sumAll([1, 4]) should return 10.
+//   sumAll([4, 1]) should return 10.
+//   sumAll([5, 10]) should return 45.
+//   sumAll([10, 5]) should return 45.
