5677b8c1f2cbd374ec0172b4962e9ae0460f6844
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-01-25 15:07:13 +0900

    Solved Problem and added comments

--- FreeCodeCampBeta/intermediateAlgorithmScripting/sumAllNumbersInARange.js
+++ FreeCodeCampBeta/intermediateAlgorithmScripting/sumAllNumbersInARange.js
@@ -5,10 +5,18 @@
 // The lowest number will not always come first.
 
 function sumAll(arr) {
-    return 1;
+  //Switch lowest number first
+  if (arr[0] > arr[1]) {
+    arr.reverse();
   }
-  
-  
+  let sum = 0;
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumAll([4, 1]));
 
 // Test Cases:
 //   sumAll([1, 4]) should return a number.
