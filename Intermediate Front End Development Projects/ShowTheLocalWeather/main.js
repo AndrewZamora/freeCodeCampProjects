53330691b48925d9afbeb1b069681a66c61c90ae
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-02-23 21:02:59 +0900

    Put loadData function in makeURL function to access lat and long variables

--- Intermediate Front End Development Projects/ShowTheLocalWeather/main.js
+++ Intermediate Front End Development Projects/ShowTheLocalWeather/main.js
@@ -1,17 +1,29 @@
-var url = "https://fcc-weather-api.glitch.me/api/current?lon=135.4009&lat= 34.7843";
 
-// Check to see if the browser supports the GeoLocation API.
-
-function loadData() {
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(makeUrl);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+getLocation();
+function makeUrl(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var url = "https://fcc-weather-api.glitch.me/api/current?lon=" + longitude + "&lat=" + latitude;
+  function loadData() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(xhttp.responseText);
-        console.log(response);      
-        // document.getElementById("demo").innerHTML = ;
+        console.log(response);
+        document.getElementById("demo").innerHTML = response.main.temp;
       }
     };
     xhttp.open("GET", url, true);
     xhttp.send();
   }
   loadData();
+}
+
+
