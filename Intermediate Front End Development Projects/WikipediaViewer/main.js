90d326ecc4a1ee4ba35508d64652620b826d5aae
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-02-27 18:20:49 +0900

    Added an h1 and form to UI

--- Intermediate Front End Development Projects/WikipediaViewer/main.js
+++ Intermediate Front End Development Projects/WikipediaViewer/main.js
@@ -1,14 +1,13 @@
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&redirects=return&search=dog&origin=*";
-    
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
+var userInput = "foot";
+var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + userInput;
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(xhttp.responseText);
         console.log(response);
         document.getElementById("demo").innerHTML = this.response;
-      }
-    };
-    xhttp.open("GET", url , true);
-    xhttp.send();
- 
-  
+    }
+};
+xhttp.open("GET", url, true);
+xhttp.send();
