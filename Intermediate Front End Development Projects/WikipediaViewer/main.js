08e79db60866c5d59cca21395fd065a877a13442
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-02-28 11:36:56 +0900

    User input is displayed

--- Intermediate Front End Development Projects/WikipediaViewer/main.js
+++ Intermediate Front End Development Projects/WikipediaViewer/main.js
@@ -1,13 +1,22 @@
-var userInput = "foot";
-var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + userInput;
+var submit = document.getElementById('search-btn');
+var demo = document.getElementById('demo');
+var userQuery = document.getElementById('user-query');
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var response = JSON.parse(xhttp.responseText);
-        console.log(response);
-        document.getElementById("demo").innerHTML = this.response;
-    }
-};
-xhttp.open("GET", url, true);
-xhttp.send();
+function loadData() {
+// Get users query from form and make url
+    var userInput = document.getElementById('search').value;
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + userInput;
+// Request Data From Wiki
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            console.log(response);
+            userQuery.innerHTML = 'Results for "' + userInput + '"';
+            demo.innerHTML = response;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+submit.addEventListener('click', loadData);
