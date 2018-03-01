fe8bf5c5ee14d4e2efa60a433964c80eeb09ae8c
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-03-01 11:37:39 +0900

    Added preventDefault() to stop form from being submited

--- Intermediate Front End Development Projects/WikipediaViewer/main.js
+++ Intermediate Front End Development Projects/WikipediaViewer/main.js
@@ -1,22 +1,26 @@
-var submit = document.getElementById('search-btn');
-var demo = document.getElementById('demo');
-var userQuery = document.getElementById('user-query');
+var submit = document.getElementById('form');
+var results = document.getElementById('result');
+var searchResult = document.getElementById('search-result');
+var searchBtn = document.getElementById('search-btn');
 
-function loadData() {
-// Get users query from form and make url
+function loadData(e) {
+    e.preventDefault();
+    // Get users query from form and make url
     var userInput = document.getElementById('search').value;
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + userInput;
-// Request Data From Wiki
+    // Request Data From Wiki
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(xhttp.responseText);
             console.log(response);
-            userQuery.innerHTML = 'Results for "' + userInput + '"';
-            demo.innerHTML = response;
+            alert(response);
+            searchResult.innerHTML = 'Results for "' + userInput + '"';
+            results.innerHTML = response;
         }
     };
     xhttp.open("GET", url, true);
     xhttp.send();
 }
-submit.addEventListener('click', loadData);
+
+submit.addEventListener('submit', loadData);
