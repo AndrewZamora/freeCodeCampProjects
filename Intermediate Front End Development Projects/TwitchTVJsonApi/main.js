8674fd276d69f667b3c4cff34894c68df3e42df3
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-03-04 22:47:49 +0900

    Refactored Http request with callback so that it could be reused with different urls

--- Intermediate Front End Development Projects/TwitchTVJsonApi/main.js
+++ Intermediate Front End Development Projects/TwitchTVJsonApi/main.js
@@ -1,30 +1,51 @@
 var demo = document.getElementById('demo');
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var url = "https://wind-bow.glitch.me/twitch-api/streams/";
-var output= '';
+var urlStreams = "https://wind-bow.glitch.me/twitch-api/streams/";
+var urlUsers = "https://wind-bow.glitch.me/twitch-api/users/";
 
-function loop (loadData){
+// Get JSON Data
+getData = function (url, callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      callback(response);
+    }
+  };
+  xhttp.open('GET', url, true);
+  xhttp.send();
+}
+// Display All User Channel Names
+function displayChannel(data) {
+  demo.innerHTML += '<div><img src="' + data.logo + '">' + '<p>' + data.display_name + '</p></div>';
+}
+
+function allUsers() {
+  var users = '';
   for (var i = 0; i < channels.length; i++) {
-    output= url + channels[i];
+    users = urlUsers + channels[i];
+    getData(users, displayChannel);
   }
-    
 }
 
+allUsers();
 
-
-function loadData(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          var response = JSON.parse(this.responseText);
-          demo.innerHTML = response.stream;
-          
-          console.log(output);
-        }
-      };
-      xhttp.open("GET", output, true);
-      xhttp.send();
-    
+// Indicate Which User Channels Are Online
+function displayOnline (data) {
+  if (data.stream == null) {
+    console.log('offline')  
+  }else{
+    console.log('online')
+  }
+  console.log(data);
 }
-loop();
 
+function onlineChannels(){
+  var streams = '';
+  for (var i = 0; i < channels.length; i++) {
+   streams = urlStreams + channels[i];
+   getData(streams, displayOnline); 
+  }
+}
+
+onlineChannels();
