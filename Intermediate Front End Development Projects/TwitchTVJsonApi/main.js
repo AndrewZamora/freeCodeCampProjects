ee822241ca92c0bf72437353224e359ca04f7556
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-03-09 23:56:29 +0900

    Pushed online channels on the DOM first

--- Intermediate Front End Development Projects/TwitchTVJsonApi/main.js
+++ Intermediate Front End Development Projects/TwitchTVJsonApi/main.js
@@ -19,31 +19,33 @@ getData = function (url, callback) {
   xhttp.send();
 }
 /////////////////////////////////////////////////
-function collectChannel(data) {
-  channelName.push(data.display_name );
-  channelName.push(data.logo);
+function checkOnline(data) {
+if(data.stream && data.stream !== null){
+  demo.innerHTML+= '<h3>'+ data.stream.channel.display_name + '</h3><div><img src="' + data.stream.channel.logo + '"><p>Online</p><p>'+ data.stream.channel.status+ '</p>';
+  channels.shift();
+  console.log(channels);
 }
-function allUsers() {
+}
+
+function getAllUsers() {
+  var stream = '';
   var users = '';
-  var streams = '';
   for (var i = 0; i < channels.length; i++) {
+    stream = urlStreams + channels[i];
     users = urlUsers + channels[i];
-    getData(users, collectChannel);
-    streams = urlStreams + channels[i];
-    getData(streams, displayOnline);
-  }
-
-}
-////////////////////////////////////////////////////////
-function displayOnline(data) {
-  if (data.stream !== null){
-    channelName.push(data);
+    getData(stream, checkOnline);
+    getData(users,checkOnline)
   }
 }
 
-/////////////////////////////////
+getAllUsers();
 
-allUsers();
+
+
+
+
+
+
 
 // Display All User Channel Names
 // function displayChannel(data) {
@@ -80,3 +82,32 @@ allUsers();
 // }
 
 // onlineChannels();
+
+
+
+
+// function collectChannel(data) {
+//   channelName.push(data.display_name );
+//   channelName.push(data.logo);
+// }
+// function allUsers() {
+//   var users = '';
+//   var streams = '';
+//   for (var i = 0; i < channels.length; i++) {
+//     users = urlUsers + channels[i];
+//     getData(users, collectChannel);
+//     streams = urlStreams + channels[i];
+//     getData(streams, displayOnline);
+//   }
+
+// }
+
+// function displayOnline(data) {
+//   if (data.stream !== null){
+//     channelName.push(data);
+//   }
+// }
+
+
+
+// allUsers();
