f4fcda1842b7dfe607efe34e9a71cc1b409130cb
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-03-18 01:01:20 +0900

    Used pop method to display offline channels

--- Intermediate Front End Development Projects/TwitchTVJsonApi/main.js
+++ Intermediate Front End Development Projects/TwitchTVJsonApi/main.js
@@ -1,57 +1,54 @@
 var demo = document.getElementById('demo');
+var offline = document.getElementById('offline');
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 var urlStreams = "https://wind-bow.glitch.me/twitch-api/streams/";
 var urlUsers = "https://wind-bow.glitch.me/twitch-api/users/";
 var channelName = [];
 var notOnline = [];
+var offlineChannels = [];
 
 /* Make Get Requests */
-getData = function (url, callback1) {
+getData = function (url, callback) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var response = JSON.parse(this.responseText);
-      callback1(response);
-      // callback2(response);
-      /* I would Like to put another callback here that is called after the first callback without using promises */
+      callback(response);
     }
   };
   xhttp.open('GET', url, true);
   xhttp.send();
 }
 
-
-/* Use channels array to make URLs to get streaming data */
+/* Make URLs to get streaming data */
 function getStreams() {
   var stream = '';
   for (var i = 0; i < channels.length; i++) {
     stream = urlStreams + channels[i];
     getData(stream, checkOnline);
   }
-
 }
-/* Use URLs to get which users that are online */
+
 function checkOnline(data) {
-  if (data.stream !== null) {
-    demo.innerHTML += '<h3>' + data.stream.channel.display_name + '</h3><div><img src="' + data.stream.channel.logo + '"><p>Online</p><p>' + data.stream.channel.status + '</p>';
-  }
+if (data.stream == null) {
+  offlineChannels.push(channels.pop());
+  getUsers();
+} else {
+  demo.innerHTML += '<h3>' + data.stream.channel.display_name + '</h3><div><img src="' + data.stream.channel.logo + '"><p>Online</p><p>' + data.stream.channel.status + '</p>';
 }
-
+}
 getStreams();
 
-function showUsers (data){
-  demo.innerHTML+= data.display_name + '<br>';
-}
-
-function getUsers (){
+function getUsers() {
   var users = '';
-  for (var i = 0; i < channels.length; i++) {
-    users = urlUsers + channels[i];
-    getData(users,showUsers);
-  }
+  users = urlUsers + offlineChannels.pop();
+  getData(users, showUsers);
+}
+function showUsers(data) {
+  offline.innerHTML += '<h3>' + data.display_name +'</h3><div><img src="'+ data.logo +'"><p>Offline</p>';
 }
 
-getUsers();
+/* Use URLs to get which users that are online */
 /* If the user is online then knock that user's channel off the channels array*/
 /* With new array make URLs to get offline user's data */
 /* Display data on the page */
