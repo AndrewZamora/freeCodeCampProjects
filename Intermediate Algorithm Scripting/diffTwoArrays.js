cdd147d2598136bfb219f1ec602043dd6c09b1db
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-03-24 23:49:20 +0900

    Got diffTwoArrays.js to work

--- Intermediate Algorithm Scripting/diffTwoArrays.js
+++ Intermediate Algorithm Scripting/diffTwoArrays.js
@@ -4,29 +4,37 @@
 // You can return the array with its elements in any order.
 
 function diffArray(arr1, arr2) {
-    var sortedArr1 = arr1;
-    var sortedArr2 = arr2;
-    var newArr = sortedArr1.filter(removeVal);
-    function removeVal(val) {
-      console.log(sortedArr2.indexOf(val) + " " + val);
-       return sortedArr2.indexOf(val) < 0;
-      }
-    return newArr;
+  var sortedArr1 = arr1;
+  var sortedArr2 = arr2;
+
+  var newArr = sortedArr1.filter(removeVal);
+  function removeVal(val) {
+    console.log(sortedArr2.indexOf(val) + " " + val);
+    return sortedArr2.indexOf(val) < 0;
   }
-  console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
-  
-  // diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
-  // ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
-  // ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with one item.
-  // ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
-  // ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with two items.
-  // ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
-  // ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return an empty array.
-  // [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
-  // [1, 2, 3, 5], [1, 2, 3, 4, 5] should return an array with one item.
-  // [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
-  // [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return an array with two items.
-  // [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
-  // [], ["snuffleupagus", "cookie monster", "elmo"] should return an array with three items.
-  // [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
-  // [1, "calf", 3, "piglet"], [7, "filly"] should return an array with six items.
+
+  var newArrTwo = sortedArr2.filter(removeValTwo);
+  function removeValTwo(val) {
+    console.log(sortedArr1.indexOf(val) + " " + val);
+    return sortedArr1.indexOf(val) < 0;
+  }
+  var all = newArr.concat(newArrTwo);
+  return all;
+}
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+// ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with one item.
+// ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+// ["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with two items.
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+// ["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return an empty array.
+// [1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+// [1, 2, 3, 5], [1, 2, 3, 4, 5] should return an array with one item.
+// [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+// [1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return an array with two items.
+// [], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+// [], ["snuffleupagus", "cookie monster", "elmo"] should return an array with three items.
+// [1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+// [1, "calf", 3, "piglet"], [7, "filly"] should return an array with six items.
