c1d3fb7d4a4595ba853aca11933c08263ca3967b
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-06-10 13:10:50 +0900

    Edited whereforeArtThou.js

--- Intermediate Algorithm Scripting/whereforeArtThou.js
+++ Intermediate Algorithm Scripting/whereforeArtThou.js
@@ -6,22 +6,31 @@
 function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
-  // Only change code below this line
-  // console.log(Object.keys(source));
-  // console.log(Object.keys(collection[0]));
-  var keyValue = Object.keys(source);
-  // console.log(source[keyValue]);
-  for (var i = 0; i < collection.length; i++) {
-    if (collection[i][keyValue] === source[keyValue]) {
-      arr.push(collection[i]);
-    }
+  var propValue = Object.keys(source);
+  // console.log(collection);
+  //  console.log(source[propValue]);
+  //  console.log(collection[i][propValue]);
+  // console.log(source[propValue]);
+  // console.log(propValue.length);
 
+  for (var i = 0; i < collection.length; i++) {
+    console.log(collection);
+    console.log(collection.hasOwnProperty(propValue[0]));
   }
   // Only change code above this line
   return arr;
 }
 
-console.log(whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }));
+console.log(whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }));
+
+
+
+//  if(source[propValue] === collection[i][propValue] && propValue.length <= 1 ){
+//       arr.push(collection[i]);
+//     } 
+//     if(collection[i][propValue[i]] === source[propValue[i]] && propValue.length > 1){
+//       arr.push(collection[i]);
+//     }
 
 
 
