a8b54d7b40a0d2e6f33c1213e601feec64bd3b22
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-06-17 16:13:17 +0900

    Solved finders keeper challenge

--- Basic Algorithmic Scripting/findersKeepers.js
+++ Basic Algorithmic Scripting/findersKeepers.js
@@ -4,10 +4,19 @@
 
 function findElement(arr, func) {
     let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i]) === true) {
+            num = arr[i];
+            return num;
+        } else {
+            num = undefined;
+        }
+    }
     return num;
   }
   
-  findElement([1, 2, 3, 4], num => num % 2 === 0);
+  console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }));
+  
 
 // Test Cases:
 
