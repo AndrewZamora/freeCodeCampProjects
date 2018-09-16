1c23f6952c9d5a2c462c872cdde3dded66cf60e1
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-09-16 12:25:39 +0900

    Solved sorted union algorithm and added comments

--- Intermediate Algorithm Scripting/sortedUnion.js
+++ Intermediate Algorithm Scripting/sortedUnion.js
@@ -5,11 +5,21 @@
 
 // The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
 
-function uniteUnique(arr) {
-    return arr;
+//Created default params with = 
+function uniteUnique(arr1,arr2=[],arr3=[],arr4=[]) {
+    //Combine all arrays 
+    let allArr = arr1.concat(arr2,arr3,arr4);
+    let result =[];
+    //Push all numbers that aren't in result
+    allArr.forEach(number => {
+        if(result.indexOf(number) === -1){
+            result.push(number);
+        }
+    });
+    return result;
 }
 
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+console.log(uniteUnique([1, 2, 3], [5, 2, 1]));
 // Test Cases:
 // uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
 // uniteUnique([1, 3, 2], [1, [5]], [2, [4]]) should return [1, 3, 2, [5], [4]].
