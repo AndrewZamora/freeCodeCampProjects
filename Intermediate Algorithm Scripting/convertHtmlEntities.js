540310ba511908afafa3ca75a861f9340077bbd1
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-09-17 10:38:42 +0900

    Added convert HTML entities algorithm

---
+++ Intermediate Algorithm Scripting/convertHtmlEntities.js
@@ -0,0 +1,9 @@
+// Intermediate Algorithm Scripting: Convert HTML Entities
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+    // &colon;&rpar;
+    return str;
+  }
+  
+console.log(convertHTML("Dolce & Gabbana"));  
