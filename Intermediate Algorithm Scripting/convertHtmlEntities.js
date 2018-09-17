39e9c4d3916ea8084fab3f126cd87d59c8d9a2a0
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-09-17 17:51:21 +0900

    Solved convert html entities

--- Intermediate Algorithm Scripting/convertHtmlEntities.js
+++ Intermediate Algorithm Scripting/convertHtmlEntities.js
@@ -2,8 +2,39 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
 function convertHTML(str) {
-    // &colon;&rpar;
-    return str;
-  }
-  
-console.log(convertHTML("Dolce & Gabbana"));  
+    let array = [...str];
+    let result = [];
+    array.forEach(letter => {
+        if (letter === '&') {
+            result.push("&amp;");
+            return;
+        };
+        if (letter === '<') {
+            result.push("&lt;");
+            return;
+        };
+        if (letter === '>') {
+            result.push("&gt;");
+            return;
+        };
+        if (letter === '"') {
+            result.push("&quot;");
+            return;
+        };
+        if (letter === "'") {
+            result.push("&apos;");
+            return;
+        }
+        result.push(letter)
+    })
+    return result.join('');
+}
+console.log(convertHTML("abc"));
+// Test Cases:
+// convertHTML("Dolce & Gabbana") should return Dolce &​amp; Gabbana.
+// convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &​lt; Pizza &​lt; Tacos.
+// convertHTML("Sixty > twelve") should return Sixty &​gt; twelve.
+// convertHTML('Stuff in "quotation marks"') should return Stuff in &​quot;quotation marks&​quot;.
+// convertHTML("Schindler's List") should return Schindler&​apos;s List.
+// convertHTML("<>") should return &​lt;&​gt;.
+// convertHTML("abc") should return abc.
