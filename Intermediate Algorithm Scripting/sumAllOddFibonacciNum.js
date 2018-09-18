53025fad9312f541814046b5de0b171a8b10e880
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-09-18 20:59:14 +0900

    Solved sum all odd fibonacci numbers algorithm and add comments

--- Intermediate Algorithm Scripting/sumAllOddFibonacciNum.js
+++ Intermediate Algorithm Scripting/sumAllOddFibonacciNum.js
@@ -6,10 +6,27 @@
 // For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 
 function sumFibs(num) {
-    return num;
+    let currentNum= 0;
+    let counter = 1;
+    let fabNums = [];
+    //Get Fibonacci Numbers
+    for (let i = 0; i < num; i++) {
+        currentNum += counter;
+        counter = currentNum-counter;
+        fabNums.push(currentNum);
+    }
+    //Get numbers that are less than num, don't repeat and odd 
+    let result =[];
+     fabNums.forEach(fabNum=>{
+        if(fabNum <= num && !result.includes(fabNum)&& fabNum % 2 !== 0){
+            result.push(fabNum);
+        }
+    })
+    //Get the sum
+    return result.reduce((a, b) => a + b) + 1;
 }
 
-console.log(sumFibs(4));
+console.log(sumFibs(4000000));
 
 // Test Cases:
 // sumFibs(1) should return a number.
