3954c09bbebc73babc70bde01ecb177f275a8384
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-10-02 11:35:58 +0900

    Got smallest common multiple to be returned but still doesn't pass tests on FCC

--- Intermediate Algorithm Scripting/smallestCommonMultiple.js
+++ Intermediate Algorithm Scripting/smallestCommonMultiple.js
@@ -6,10 +6,33 @@
 // For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
 
 function smallestCommons(arr) {
-    return arr;
+    //Sort argument from greatest to least
+    const sorted =arr.sort((a,b)=>a - b);
+    //Create the range of numbers from argument
+    let range = []
+    let result="";
+    for (let i = sorted[0]; i <= sorted[1]; i++) {
+        range.push(i);
+    }
+    //Make a function that checks for divisible numbers
+    function noRemainder(num){
+        for (let i = 0; i < range.length; i++) {
+            if(num % range[i] !== 0){
+                return false;
+            }
+        }
+        return num;
+    }
+    // Multiply big number
+    for (let i = 0; result.length < 1; i++) {
+       if(noRemainder(range[range.length-1]*i)) {
+         result = noRemainder(range[range.length-1]*i)
+       }
+    }
+    return result
   }
 
-console.log(smallestCommons([1,5]));
+console.log(smallestCommons([23,18]))
 // Test Cases:
 // smallestCommons([1, 5]) should return a number.
 // smallestCommons([1, 5]) should return 60.
