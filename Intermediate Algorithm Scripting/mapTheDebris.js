2b7964661f1e1facfb3a9095ee1c43276d648215
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-10-20 17:08:20 +0900

    Solved map the debris algorithm

--- Intermediate Algorithm Scripting/mapTheDebris.js
+++ Intermediate Algorithm Scripting/mapTheDebris.js
@@ -5,12 +5,22 @@
 // The values should be rounded to the nearest whole number. The body being orbited is Earth.
 // The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
 function orbitalPeriod(arr) {
-    var GM = 398600.4418;
-    var earthRadius = 6367.4447;
-    return arr;
-  }
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    let answer = []
+    function getPeriod(x) {
+        let altitude = x.avgAlt;
+        let arrName = x.name;
+        let result = Math.round(Math.sqrt(Math.pow((earthRadius + altitude), 3) / GM) * (2 * Math.PI));
+        answer.push({ name: arrName, orbitalPeriod: result });
+    }
+    arr.forEach(element => {
+        getPeriod(element);
+    });
+    return answer;
+}
 
-console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
+console.log(orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]));
 
 //Test Cases:
 // orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]) should return [{name: "sputnik", orbitalPeriod: 86400}].
