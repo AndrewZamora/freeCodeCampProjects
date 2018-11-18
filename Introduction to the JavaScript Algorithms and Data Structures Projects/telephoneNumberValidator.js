b33cc956149964970f03382661a67274a6aa3af0
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-11-18 19:57:50 +0900

    Solved telephone number validator algorithm

--- Introduction to the JavaScript Algorithms and Data Structures Projects/telephoneNumberValidator.js
+++ Introduction to the JavaScript Algorithms and Data Structures Projects/telephoneNumberValidator.js
@@ -10,12 +10,14 @@
 // 5555555555
 // 1 555 555 5555
 // For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
+
 function telephoneCheck(str) {
-    // Good luck!
-    return true;
+    let regex =/^1?\s?(\(\d{3}\)|\d{3})\s?-?\d{3}\s?-?\d{4}$/g;
+    let valid = regex.test(str)
+    return valid;
   }
 
-  telephoneCheck("555-555-5555");
+console.log(telephoneCheck("1 555-555-5555"));
 // Test Cases:
 // telephoneCheck("555-555-5555") should return a boolean.
 // telephoneCheck("1 555-555-5555") should return true.
