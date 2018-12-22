5bbdbbb30c68d2db5162ac82f3928261b568e70d
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-12-22 23:22:32 +0900

    solved cash register algorithm

--- Introduction to the JavaScript Algorithms and Data Structures Projects/cashRegister.js
+++ Introduction to the JavaScript Algorithms and Data Structures Projects/cashRegister.js
@@ -14,22 +14,77 @@
 
 
 function checkCashRegister(price, cash, cid) {
-    var change;
-    return change;
-  }
+    const cashValues = [
+        { name: "PENNY", value: 0.01 },
+        { name: "NICKEL", value: 0.05 },
+        { name: "DIME", value: 0.1 },
+        { name: "QUARTER", value: 0.25 },
+        { name: "ONE", value: 1 },
+        { name: "FIVE", value: 5 },
+        { name: "TEN", value: 10 },
+        { name: "TWENTY", value: 20 },
+        { name: "ONE HUNDRED", value: 100 }
+    ];
 
-  // Example cash-in-drawer array:
-  // [["PENNY", 1.01],
-  // ["NICKEL", 2.05],
-  // ["DIME", 3.1],
-  // ["QUARTER", 4.25],
-  // ["ONE", 90],
-  // ["FIVE", 55],
-  // ["TEN", 20],
-  // ["TWENTY", 60],
-  // ["ONE HUNDRED", 100]]
+    let insufficient = { status: "INSUFFICIENT_FUNDS", change: [] };
+    let closed = { status: "CLOSED", change: cid };
+    let open = { status: "OPEN", change: [] };
+    // Get cash in drawer total and change due
+    let changeDue = cash - price;
+    let cidTotal = 0;
+    for (let i = 0; i < cid.length; i++) {
+        cidTotal += cid[i][1];
+    }
+    //Check for insufficient or closed
+    if (cidTotal < changeDue) {
+        return insufficient;
+    }
+    if (cidTotal == changeDue) {
+        return closed;
+    }
+    // Get change
+    //Find highest denomination to break up change due
+    let highestDenom;
+    for (let j = 0; j < cashValues.length; j++) {
+        if (changeDue < cashValues[j].value) {
+            highestDenom = j - 1;
+            break
+        }
+    }
+    //Round money
+    function round(value) {
+        return Math.round(value * 100) / 100;
+    }
+    // Distribute change
+    let counter = 0;
+    function distribute(moneyName, moneyValue, cidLimit, payBack) {
+        let result = 0;
+        while (result < cidLimit && counter < payBack) {
+            if (counter + moneyValue > payBack) {
+                if (round(counter + moneyValue) === payBack) {
+                    result += moneyValue
+                    counter += moneyValue
+                }
+                break
+            }
+            result += moneyValue;
+            counter += moneyValue;
+        }
+        if (result !== 0) {
+            open.change.push([moneyName, result]);
+        }
+    }
+    for (let k = highestDenom; k >= 0; k--) {
+        distribute(cashValues[k].name, cashValues[k].value, cid[k][1], changeDue)
+    }
+    //Check for cash in drawer that could not be broken up
+    if (round(counter) !== changeDue) {
+        return insufficient
+    }
+    return open;
+}
 
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 
 // Test Cases:
 // checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
