8d850418cae10c754d240526ea57a63fe2d98fb3
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-01-10 23:30:50 +0900

    Added label component

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Label from './components/Label';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div>Hello</div>
+      <div style={styles.container}>
+        <Label id="break-label">Break Length</Label>
+        <Label id="session-label">Session Length</Label>
       </div>
     );
   }
 }
 
 export default App;
+
+const styles = {
+  container:{
+    display:'flex',
+    flexDirection:'column'
+  }
+}
