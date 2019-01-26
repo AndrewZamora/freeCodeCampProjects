b9ec689837c23a9712a01b13fce8e1d19a568ba3
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-01-26 21:28:01 +0900

    Made clock component just take min input and moved functions to app.js

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
 import Clock from './components/Clock';
+import Input from './components/Input';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      breakLength: 2,
+      sessionLength: 5,
+      interval: '',
+      switchTime: false,
+      timer: 5
+    };
+  }
+  countDown = () => {
+    this.setState(({
+      timer: this.state.timer -= 1,
+    }));
+    if (this.state.timer < 0 && !this.state.switchTime) {
+      this.setState(({
+        timer: this.state.breakLength,
+        switchTime: !this.state.switchTime
+      }));
+    }
+    if (this.state.timer < 0 && this.state.switchTime === true) {
+      this.setState(({
+        timer: this.state.sessionLength,
+        switchTime: !this.state.switchTime
+      }));
+    }
+  }
+  start = () => {
+    this.setState(({
+      interval: setInterval(this.countDown, 1000),
+      timer: this.state.sessionLength
+    }));
+  }
+  pause = () => {
+    clearInterval(this.state.interval);
+  }
+  handleSessionInput = input => {
+    // timer also need to be set so that user can see the change
+    this.setState(({
+      sessionLength: input.value,
+      timer: input.value
+    }));
+  }
+  handleBreakInput = input => {
+    this.setState(({
+      breakLength: input.value
+    }));
+  }
   render() {
     return (
       <div style={styles.container}>
-        <Clock status="Work Time"/>
+        <Clock time={this.state.timer} />
+        <div style={styles.inputContainer}>
+          <Input max={59} unit="min" func={(event) => this.handleSessionInput(event)}></Input>
+          <Input max={59} unit="min" func={(event) => this.handleBreakInput(event)}></Input>
+        </div>
+        <button onClick={() => this.start()}>Start</button>
+        <button onClick={() => this.pause()}>Pause</button>
       </div>
     );
   }
@@ -18,5 +73,8 @@ const styles = {
   container: {
     display: 'flex',
     flexDirection: 'column'
+  },
+  inputContainer: {
+    display: 'flex'
   }
 }
--- pomodoro-clock/src/components/Clock.js
+++ pomodoro-clock/src/components/Clock.js
@@ -3,101 +3,13 @@ import Input from './Input';
 import Label from './Label';
 
 class Clock extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            time: { hour: 0, min: 0, sec: 0 },
-            units: ['hour', 'min', 'sec'],
-            interval: ''
-        };
-    }
-    handleInput = input => {
-        const { units } = this.state;
-        units.forEach(unit => {
-            if (input['unit'].includes(unit)) {
-                this.setState(({
-                    time: {
-                        ...this.state.time,
-                        [input.unit]: input.value
-                    }
-                }))
-            }
-        })
-    }
-    timer = () => {
-        let time = Object.assign({}, this.state.time);
-        let { hour, min, sec } = time;
-        if (hour > 0 && sec < 1 && min < 1) { hour -= 1 }
-        if (min === 59) { hour -= 1 }
-        if (hour === 0 && sec === 0) { min -= 1; }
-        if (sec === 0) { sec = 60; }
-        sec -= 1;
-        if (hour + min + sec === 0) { clearInterval(this.state.interval); }
-        this.setState(({
-            time: {
-                hour: hour,
-                min: min,
-                sec: sec
-            }
-        }))
-    }
-    start = () => {
-        this.setState(({
-            interval: setInterval(this.timer, 100)
-        }))
-    }
-    pause = () => {
-        clearInterval(this.state.interval);
-    }
-    reset = () => {
-        clearInterval(this.state.interval);
-        this.setState(({
-            time: {
-                hour: 0,
-                min: 0,
-                sec: 0
-            },
-            interval: ''
-        }))
-    }
-    addZero = i => {
-        if (i < 10) { i = "0" + i; }
-        return i;
-    }
     render() {
-        const time = Object.keys(this.state.time).map(i => this.addZero(this.state.time[i]));
         return (
             <div>
-                <h2>{this.state.status}</h2>
-                <p style={styles.p}>{`${time[0]}:${time[1]}:${time[2]}`}</p>
-                <Label id="break-label">Break Length</Label>
-                <div style={styles.inputContainer}>
-                    <Input max={99} unit="hour" func={(event) => this.handleInput(event)}></Input>
-                    <Input max={59} unit="min" func={(event) => this.handleInput(event)}></Input>
-                    <Input max={59} unit="sec" func={(event) => this.handleInput(event)}></Input>
-                </div>
-                <div style={styles.buttonsContainer}>
-                    <button onClick={() => this.start()}>Start</button>
-                    <button onClick={() => this.pause()}>Pause</button>
-                    <button onClick={() => this.reset()}>Reset</button>
-                </div>
-                <Label id="session-label">Session Length</Label>
+                {this.props.time}
             </div>
         );
     }
 }
 
 export default Clock;
-
-const styles = {
-    p: {
-        fontSize: '5em'
-    },
-    inputContainer: {
-        display: 'flex',
-        justifyContent: 'center'
-    },
-    buttonsContainer: {
-        display: 'flex'
-    }
-}
