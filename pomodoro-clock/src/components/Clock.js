66e60a238085d0e04fd70b46d5decef68bd946d6
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-01-29 19:58:19 +0900

    Added formatTime function to clock component

--- pomodoro-clock/src/components/Clock.js
+++ pomodoro-clock/src/components/Clock.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 class Clock extends Component {
+    formatTime = (milSec) => {
+       return new Date(milSec).toISOString().substr(14, 5);
+    }
     render() {
         return (
-            <div style={{textAlign:'center'}}>
-                {this.props.time}
+            <div style={{ textAlign: 'center' }}>
+                {this.formatTime(this.props.time)}
             </div>
         );
     }
