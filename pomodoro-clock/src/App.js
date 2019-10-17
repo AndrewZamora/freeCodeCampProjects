cfd03d44374804b3eba3a0e13407b17fc840165e
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-10-18 02:38:34 +0900

    Refactored countDown function and removed clock component

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Clock from './components/Clock';
 
 class App extends Component {
   constructor(props) {
@@ -11,32 +10,23 @@ class App extends Component {
       sessionLength: 1500000,
       status: "Session",
       interval: '',
-      switchTime: false,
       timer: 1500000,
       initialized: false,
       start: false
     };
   }
   countDown = () => {
-    this.setState(prevState => ({
-      timer: prevState.timer - 1000
-    }));
-    if (this.state.timer === 0 && !this.state.switchTime) {
+    if (this.state.timer === 0) {
       this.beep.current.play();
       this.setState(({
         timer: this.state.breakLength,
-        switchTime: !this.state.switchTime,
-        status: "Break"
+        status: this.state.status === "Session" ? "Break" : "Session"
       }));
-    }
-    if (this.state.timer === 0 && this.state.switchTime) {
-      this.beep.current.play();
-      this.setState(({
-        timer: this.state.sessionLength,
-        switchTime: !this.state.switchTime,
-        status: "Session"
-      }));
-    }
+      return;
+    };
+    this.setState({
+      timer: this.state.timer - 1000
+    });
   }
   start = () => {
     if (!this.state.initialized) {
@@ -74,7 +64,7 @@ class App extends Component {
     if (this.state[length] < 3600000) {
       this.setState(prevState => ({
         [length]: prevState[length] + 60000,
-        timer: length === "sessionLength" ?  prevState[length] + 60000 : prevState.timer
+        timer: length === "sessionLength" ? prevState[length] + 60000 : prevState.timer
       }));
     }
   }
@@ -82,9 +72,12 @@ class App extends Component {
     if (this.state[length] > 60000) {
       this.setState(prevState => ({
         [length]: prevState[length] - 60000,
-        timer: length === "sessionLength" ?  prevState[length] - 60000 : prevState.timer
+        timer: length === "sessionLength" ? prevState[length] - 60000 : prevState.timer
       }));
     }
+  }
+  formatTime = milSec => {
+    return new Date(milSec).toISOString().substr(14, 5);
   }
   render() {
     return (
@@ -92,23 +85,25 @@ class App extends Component {
         <h1>Pomodoro Clock</h1>
         <div>
           <p>Status: <span id="timer-label">{this.state.status}</span></p>
-          <Clock time={this.state.timer} />
+          <div style={{ textAlign: 'center' }} id="time-left">
+            {`${this.formatTime(this.state.timer)}`}
+          </div>
         </div>
         <div>
           <h3 id="break-length">{this.state.breakLength / 60000}</h3>
-          <label htmlFor="" id="break-label">Break Length</label>
+          <label id="break-label">Break Length</label>
           <button id="break-decrement" onClick={() => this.decrement("breakLength")}>-</button>
           <button id="break-increment" onClick={() => this.increment("breakLength")}>+</button>
         </div>
         <div>
           <h3 id="session-length">{this.state.sessionLength / 60000}</h3>
-          <label htmlFor="" id="session-label">Session Length</label>
+          <label id="session-label">Session Length</label>
           <button id="session-decrement" onClick={() => this.decrement("sessionLength")}>-</button>
           <button id="session-increment" onClick={() => this.increment("sessionLength")}>+</button>
         </div>
         <button id="start_stop" onClick={() => this.start()}> {this.state.start ? "Pause" : "Start"}</button>
         <button id="reset" onClick={() => this.reset()}>Reset</button>
-        <audio ref={this.beep} id="beep" src="https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3"/>
+        <audio ref={this.beep} id="beep" src="https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3" />
       </div>
     );
   }
